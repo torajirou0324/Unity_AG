f307a46d187a046c7f27d67b9d419bbfa305b0e6
tarou <tatou@outlook.jp>
2022-10-01 19:45:11 +0900

    プレイヤーとエネミーとステージ　プレイヤーは移動制御完了

---
+++ BloodSeizure/Assets/Shougo/Script/PlayerController.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float animSpeed = 1.5f;				// アニメーション再生速度設定
+
+    // 前進速度
+    public float forwardSpeed = 7.0f;
+    // 後退速度
+    public float backwardSpeed = 2.0f;
+    // 旋回速度
+    public float rotateSpeed = 2.0f;
+    // ジャンプ威力
+    public float jumpPower = 3.0f;
+
+    private Rigidbody rb;
+    private Animator anim;                          // キャラにアタッチされるアニメーターへの参照
+    private AnimatorStateInfo currentBaseState;         // base layerで使われる、アニメーターの現在の状態の参照
+
+    private GameObject cameraObject;    // メインカメラへの参照
+    // アニメーター各ステートへの参照
+    static int idleState = Animator.StringToHash("Base Layer.Idle");
+    static int locoState = Animator.StringToHash("Base Layer.Locomotion");
+    static int jumpState = Animator.StringToHash("Base Layer.Jump");
+    static int restState = Animator.StringToHash("Base Layer.Rest");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Animatorコンポーネントを取得する
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        //メインカメラを取得する
+        cameraObject = GameObject.FindWithTag("MainCamera");
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");              // 入力デバイスの水平軸をhで定義
+        float v = Input.GetAxis("Vertical");                // 入力デバイスの垂直軸をvで定義
+        anim.SetFloat("Speed", v);                          // Animator側で設定している"Speed"パラメタにvを渡す
+        anim.SetFloat("Direction", h);                      // Animator側で設定している"Direction"パラメタにhを渡す
+        anim.speed = animSpeed;                             // Animatorのモーション再生速度に animSpeedを設定する
+        currentBaseState = anim.GetCurrentAnimatorStateInfo(0); // 参照用のステート変数にBase Layer (0)の現在のステートを設定する
+        rb.useGravity = true;//ジャンプ中に重力を切るので、それ以外は重力の影響を受けるようにする
+
+        Vector3 velocity = new Vector3(0, 0, v);        // 上下のキー入力からZ軸方向の移動量を取得
+                                                        // キャラクターのローカル空間での方向に変換
+        velocity = transform.TransformDirection(velocity);
+        //以下のvの閾値は、Mecanim側のトランジションと一緒に調整する
+        if (v > 0.1)
+        {
+            velocity *= forwardSpeed;       // 移動速度を掛ける
+        }
+        else if (v < -0.1)
+        {
+            velocity *= backwardSpeed;  // 移動速度を掛ける
+        }
+
+        // 上下のキー入力でキャラクターを移動させる
+        transform.localPosition += velocity * Time.fixedDeltaTime;
+
+        // 左右のキー入力でキャラクタをY軸で旋回させる
+        transform.Rotate(0, h * rotateSpeed, 0);
+    }
+
+    void Update()
+    {
+        if (currentBaseState.nameHash == locoState)
+        {
+
+        }
+        // JUMP中の処理
+        // 現在のベースレイヤーがjumpStateの時
+        else if (currentBaseState.nameHash == jumpState)
+        {
+            // ステートがトランジション中でない場合
+            if (!anim.IsInTransition(0))
+            {
+                // Jump bool値をリセットする（ループしないようにする）				
+                anim.SetBool("Jump", false);
+            }
+        }
+        // IDLE中の処理
+        // 現在のベースレイヤーがidleStateの時
+        else if (currentBaseState.nameHash == idleState)
+        {
+            // スペースキーを入力したらRest状態になる
+            if (Input.GetButtonDown("Jump"))
+            {
+                anim.SetBool("Rest", true);
+            }
+        }
+        else if (currentBaseState.nameHash == restState)
+        {
+            //cameraObject.SendMessage("setCameraPositionFrontView");		// カメラを正面に切り替える
+            // ステートが遷移中でない場合、Rest bool値をリセットする（ループしないようにする）
+            if (!anim.IsInTransition(0))
+            {
+                anim.SetBool("Rest", false);
+            }
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {   // スペースキーを入力したら
+
+            //アニメーションのステートがLocomotionの最中のみジャンプできる
+            if (currentBaseState.nameHash == locoState)
+            {
+                //ステート遷移中でなかったらジャンプできる
+                if (!anim.IsInTransition(0))
+                {
+                    rb.AddForce(Vector3.up * jumpPower, ForceMode.VelocityChange);
+                    anim.SetBool("Jump", true);     // Animatorにジャンプに切り替えるフラグを送る
+                }
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(1);
+        if (collision.gameObject.name == "Enemy")
+        {
+            rb.AddForce(Vector3.up * jumpPower, ForceMode.VelocityChange);
+            Debug.Log(2);
+        }
+    }
+}
